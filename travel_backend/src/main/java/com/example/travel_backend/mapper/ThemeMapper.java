93990157b498a820e84574a2202e92937fe1493d
Lee jae hyeong <ftko6252@naver.com>
2024-06-05 13:10:13 +0900

    api

--- travel_backend/src/main/java/com/example/travel_backend/mapper/ThemeMapper.java
+++ travel_backend/src/main/java/com/example/travel_backend/mapper/ThemeMapper.java
@@ -12,8 +12,18 @@ import java.util.List;
 public interface ThemeMapper {
     List<TourMainDTO> selectAllThemes(@Param("count") int count, @Param("offset") int offset);
 
-    List<TourMainDTO> selectByTheme(@Param("areaCode") String areaCode, @Param("count") int count, @Param("offset") int offset,
-                                    @Param("cat1") String cat1, @Param("cat2") String cat2, @Param("cat3") String cat3, @Param("random") boolean random);
+    List<TourMainDTO> selectByTheme(@Param("areaCode") String areaCode,
+                                    @Param("count") int count,
+                                    @Param("offset") int offset,
+                                    @Param("cat1") String cat1,
+                                    @Param("cat2") String cat2,
+                                    @Param("cat3") String cat3,
+                                    @Param("random") boolean random);
+
+    int countByTheme(@Param("areaCode") String areaCode,
+                     @Param("cat1") String cat1,
+                     @Param("cat2") String cat2,
+                     @Param("cat3") String cat3);
 
     TourDTO selectByContentId(@Param("contentId") String contentId);
 }
