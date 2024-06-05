a6ab6ee8f920962271cb91f0baf39650b6e45b84
jun <skyjoon2@naver.com>
2024-06-05 19:10:31 +0900

    login수정

--- travel_backend/src/test/java/com/example/travel_backend/TravelBackendApplicationTests.java
+++ travel_backend/src/test/java/com/example/travel_backend/TravelBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.travel_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TravelBackendApplicationTests {
 
 	@Test
