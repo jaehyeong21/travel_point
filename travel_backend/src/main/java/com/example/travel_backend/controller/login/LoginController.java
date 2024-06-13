240ab3625036492616259c06046bee433fdbbf80
jun <skyjoon2@naver.com>
2024-06-13 16:49:08 +0900

    로그인, 회원가입시 jwt토큰 발급

--- travel_backend/src/main/java/com/example/travel_backend/controller/login/LoginController.java
+++ travel_backend/src/main/java/com/example/travel_backend/controller/login/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.travel_backend.controller.Login;
+package com.example.travel_backend.controller.login;
 
 import com.example.travel_backend.data.LoginDto;
 import com.example.travel_backend.jwt.JwtToken;
@@ -16,132 +16,130 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Optional;
 @RestController("loginController")
 public class LoginController {
 
     @Autowired
     private MemberRepository memberRepository;
+
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     private MemberService memberService;
 
-//    //로그인
-//    @Operation(summary = "로그인", description = "userEmail과 password를 입력받아, 로그인을 진행합니다.")
-//    @PostMapping("/loginForm")
-//    public ResponseEntity<?> loginForm(@RequestBody Map<String, String> loginData) {
-//        String userEmail = loginData.get("email");
-//        String password = loginData.get("password");
-
-//        Member member = memberRepository.findByEmail(userEmail);
-//
-//        if (member == null) {
-//            Map<String, Object> response = new HashMap<>();
-//            response.put("response", false);
-//            response.put("message", "Invalid Email");
-//            response.put("errorCode", "AUTH001");
-//            return ResponseEntity.badRequest().body(response);
-//        }
-//
-//        // 이메일이 존재하므로 비밀번호를 검증
-//        if (!bCryptPasswordEncoder.matches(password, member.getPassword())) {
-//            Map<String, Object> response = new HashMap<>();
-//            response.put("response", false);
-//            response.put("message", "Invalid Password");
-//            response.put("errorCode", "AUTH001");
-//            return ResponseEntity.badRequest().body(response);
-//        }
-//
-//        Map<String, Object> result = new HashMap<>();
-//        result.put("id", member.getId());
-//        result.put("createDate", member.getCreateDate());
-//        result.put("userName", member.getUsername());
-//        result.put("userImgUrl", member.getUserImgUrl());
-//        result.put("userEmail", userEmail);
-//
-//
-//        Map<String, Object> response = new HashMap<>();
-//        response.put("response", "ok");
-//        response.put("result", result);
-//        response.put("token", "test");
-//
-//        return ResponseEntity.ok(response);
-//    }
+    //로그인
     @Operation(summary = "로그인", description = "userEmail과 password를 입력받아, 로그인을 진행합니다.")
     @PostMapping("/loginForm")
-    public JwtToken login(@RequestBody LoginDto loginDto){
+    public ResponseEntity<?> loginForm(@RequestBody LoginDto loginDto) {
         String userEmail = loginDto.getEmail();
         String password = loginDto.getPassword();
-        JwtToken jwtToken = memberService.login(userEmail,password);
 
-        return jwtToken;
+        Optional<Member> memberOptional = memberRepository.findByEmail(userEmail);
+
+        if (!memberOptional.isPresent()) {
+            return ResponseEntity.badRequest().body(createErrorResponse("Invalid Email", "AUTH001"));
+        }
+
+        Member member = memberOptional.get();
+
+        // 이메일이 존재하므로 비밀번호를 검증
+        if (!bCryptPasswordEncoder.matches(password, member.getPassword())) {
+            return ResponseEntity.badRequest().body(createErrorResponse("Invalid Password", "AUTH002"));
+        }
+
+        // JWT 토큰 생성
+        JwtToken jwtToken = memberService.login(userEmail, password);
+
+        // 응답 데이터 생성
+        Map<String, Object> response = new HashMap<>();
+        response.put("response", "ok");
+        response.put("result", createLoginResponse(member, jwtToken));
+//        response.put("token", jwtToken);
+
+        return ResponseEntity.ok(response);
     }
 
-    //스프링시큐리티 해당주소를 낚아채버린다. -> SecurityConfig 파일 생성 후 작동안함.
-    //회원가입
+    // 회원가입
     @Operation(summary = "회원가입", description = "userEmail과 password를 입력받아, 회원가입을 진행합니다.")
-    @PostMapping("/join") //joinForm에서 Post방식으로 보냄
+    @PostMapping("/join")
     public ResponseEntity<?> join(@RequestBody Member member) {
         // 이메일 형식을 검증
         if (!EmailValidator.isValidEmail(member.getEmail())) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("response", false);
-            response.put("message", "Invalid Email");
-            response.put("errorCode", "EmailError");
-
-            Map<String, Object> result = new HashMap<>();
-            result.put("member", member);
-            response.put("result", result);
-            return ResponseEntity.badRequest().body(response);
+            return ResponseEntity.badRequest().body(createErrorResponse("Invalid Email", "EmailError"));
         }
         // 패스워드 형식을 검증
         if (!PasswordValidator.isValid(member.getPassword())) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("response", false);
-            response.put("message", "Invalid Password");
-            response.put("errorCode", "PasswordError");
-
-            Map<String, Object> result = new HashMap<>();
-            result.put("member", member);
-            response.put("result", result);
-            return ResponseEntity.badRequest().body(response);
+            return ResponseEntity.badRequest().body(createErrorResponse("Invalid Password", "PasswordError"));
         }
 
-        member.setRole("ROLE_USER");
+        // 패스워드 인코딩
         String rawPassword = member.getPassword();
         String encPassword = bCryptPasswordEncoder.encode(rawPassword);
         member.setPassword(encPassword);
 
+        // 회원 저장
+        member.setRole("USER");
         memberRepository.save(member);
 
-        String userImgUrl = "http://localhost:8080/assets/image/characters/anonymous.png";
+        // JWT 토큰 생성
+        JwtToken jwtToken = memberService.login(member.getEmail(), rawPassword);
+
+        // 회원 정보 응답 생성
+        Map<String, Object> response = new HashMap<>();
+        response.put("response", "ok");
+        response.put("result", createJoinResponse(member, jwtToken));
+
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 회원가입 응답 생성
+    private Map<String, Object> createJoinResponse(Member member, JwtToken jwtToken) {
+
+        String userImgUrl = "/assets/image/characters/anonymous.png";
 
         if (member.getUserImgUrl() == null || member.getUserImgUrl().isEmpty()) {
             member.setUserImgUrl(userImgUrl);
         }
 
-        Map<String, Object> memberMap = new HashMap<>();
-        memberMap.put("id", member.getId());
-        memberMap.put("createDate", member.getCreateDate());
-        memberMap.put("username", member.getUsername());
-        memberMap.put("userImgUrl", member.getUserImgUrl());
-        memberMap.put("email", member.getEmail());
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("id", member.getId());
+        userMap.put("createDate", member.getCreateDate());
+        userMap.put("username", member.getUsername());
+        userMap.put("userImgUrl", userImgUrl);
+        userMap.put("email", member.getEmail());
 
         Map<String, Object> result = new HashMap<>();
-        result.put("user", memberMap);
-        result.put("token", "jwt-token-string");  // 여기에 실제 JWT 토큰 생성 및 설정
+        result.put("user", userMap);
+        result.put("token", jwtToken);
 
+        return result;
+    }
+
+    // 로그인 응답 생성
+    private Map<String, Object> createLoginResponse(Member member, JwtToken jwtToken) {
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("id", member.getId());
+        userMap.put("createDate", member.getCreateDate());
+        userMap.put("username", member.getUsername());
+        userMap.put("userImgUrl", member.getUserImgUrl());
+        userMap.put("email", member.getEmail());
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("user", userMap);
+        result.put("token", jwtToken);
+
+        return result;
+    }
+
+    // 에러 응답 생성
+    private Map<String, Object> createErrorResponse(String message, String errorCode) {
         Map<String, Object> response = new HashMap<>();
-        response.put("response", true);
-        response.put("result", result);
-
-        return ResponseEntity.ok(response);
+        response.put("response", false);
+        response.put("message", message);
+        response.put("errorCode", errorCode);
+        return response;
     }
-    @PostMapping("/test")
-    public String test(){
-        return "success";
-    }
-
 }
